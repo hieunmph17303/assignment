d92fd90017592d23738d374161a769cb1bbf2cb8
hieu <hieunmph17303@fpt.edu.vn>
2022-01-07 10:31:16 +0700

    fix data

--- src/data.js
+++ src/data.js
@@ -1,4 +1,4 @@
-const menu = [
+export const menu = [
     "Trang chủ",
     "Tuyển sinh",
     "Chương trình đào tạo",
@@ -6,18 +6,19 @@ const menu = [
     "Tuyển dụng",
 ];
 
-const news = [{
-        img: "https://picsum.photos/896/250",
+export const news = [
+    {
+        img: "https://picsum.photos/250/200",
         title: "Vinh danh 295 sinh viên FPT Polytecnic học kỳ Srping 2018",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam ex, saepe possimus sequi quibusdam vitae labore voluptas?",
     },
     {
-        img: "https://picsum.photos/896/250",
+        img: "https://picsum.photos/250/200",
         title: "Vinh danh 295 sinh viên FPT Polytecnic học kỳ Srping 2018",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam ex, saepe possimus sequi quibusdam vitae labore voluptas?",
     },
     {
-        img: "https://picsum.photos/896/250",
+        img: "https://picsum.photos/250/200",
         title: "Vinh danh 295 sinh viên FPT Polytecnic học kỳ Srping 2018",
         description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam ex, saepe possimus sequi quibusdam vitae labore voluptas?",
     },
--- src/main.js
+++ src/main.js
@@ -8,3 +8,31 @@
 // });
 
 // router.resolve();
+
+import { menu, news } from "./data";
+
+const menuE = document.querySelector(".menu");
+if (menuE) {
+    const result = menu.map((item) => ` <li class="menu-item">
+                            <a href="" class="menu-item__link">${item}</a>
+                        </li>`).join("");
+    menuE.innerHTML = result;
+}
+
+const newsListE = document.querySelectorAll(".news-list");
+if (newsListE) {
+    const result = news.map(({ img, title, description }) => `
+    <div class="p-4 border-solid border border-[#c2c2c2]">
+        <a href="">
+            <img src="${img}" alt="">
+            <h3 class="text-[#ae3e00] text-lg font-bold py-2">${title}
+            </h3>
+            <p class="text-sm">${description}
+            </p>
+        </a>
+    </div>`).join("");
+
+    newsListE.forEach((item) => {
+        item.innerHTML = result;
+    });
+}
