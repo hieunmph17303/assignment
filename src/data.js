4d211a32f2fdb208c788c739ca723af2cdb3fa61
hieu <hieunmph17303@fpt.edu.vn>
2022-01-07 09:46:24 +0700

    init project

---
+++ src/data.js
@@ -0,0 +1,24 @@
+const menu = [
+    "Trang chủ",
+    "Tuyển sinh",
+    "Chương trình đào tạo",
+    "Góc tuyển sinh",
+    "Tuyển dụng",
+];
+
+const news = [{
+        img: "https://picsum.photos/896/250",
+        title: "Vinh danh 295 sinh viên FPT Polytecnic học kỳ Srping 2018",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam ex, saepe possimus sequi quibusdam vitae labore voluptas?",
+    },
+    {
+        img: "https://picsum.photos/896/250",
+        title: "Vinh danh 295 sinh viên FPT Polytecnic học kỳ Srping 2018",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam ex, saepe possimus sequi quibusdam vitae labore voluptas?",
+    },
+    {
+        img: "https://picsum.photos/896/250",
+        title: "Vinh danh 295 sinh viên FPT Polytecnic học kỳ Srping 2018",
+        description: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquam ex, saepe possimus sequi quibusdam vitae labore voluptas?",
+    },
+];
---
+++ src/main.js
@@ -0,0 +1,10 @@
+// import Navigo from "navigo";
+
+// const router = new Navigo("/", { linksSelector: "a" });
+
+// router.on({
+//     "/": () => console.log("home page"),
+//     "/about": () => console.log("about page"),
+// });
+
+// router.resolve();
